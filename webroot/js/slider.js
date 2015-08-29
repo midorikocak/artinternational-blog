a6806d7fd7f3954d2895cb8dd00ab8243c6b4fd1
Midori Koçak <mtkocak@gmail.com>
2015-08-29 21:06:00 +0200

    font size

--- webroot/js/slider.js
+++ webroot/js/slider.js
@@ -1,4 +1,6 @@
-
-    $(document).ready(function(){
-      $('.featured-slider').slick();
+$(document).ready(function(){
+    $('.featured-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000
     });
+});
